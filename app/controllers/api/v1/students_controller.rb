051b4ede52a8ba3f2bba35cc42181f8b6271163a
Danilo Castro <danilogcastro@hotmail.com>
2021-07-16 22:36:49 -0300

    index with correct formatting

--- app/controllers/api/v1/students_controller.rb
+++ app/controllers/api/v1/students_controller.rb
@@ -2,10 +2,15 @@ class Api::V1::StudentsController < ApplicationController
   def index
     @pagy, @students = pagy(Student.all, items: params[:count])
     render json: { page: params[:page],
-                  items: @students 
+                  items: @students.as_json 
                  }, status: 200
   end
 
   def create
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:name, :cpf, :birthdate, :payment_method)
   end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,6 @@
 class Student < ApplicationRecord
+  after_save :format_cpf
+
   PAYMENT_METHODS = ["boleto", "credit_card"]
   has_many :enrollments
 
@@ -6,4 +8,28 @@ class Student < ApplicationRecord
   validates :cpf, presence: true #, uniqueness: true
   validates :payment_method, inclusion: { in: PAYMENT_METHODS, message: "forma de pagamento deve ser válida" }
   validates_with CpfValidator
+
+  def as_json(options = {})
+    {
+      id: id,
+      name: name,
+      cpf: cpf,
+      birthdate: birthdate.to_s,
+      payment_method: translated_payment
+    }
+  end
+  
+  private
+  def format_cpf
+    formatted_cpf = "#{self.cpf[0..2]}.#{self.cpf[3..5]}.#{self.cpf[6..8]}-#{self.cpf[9..10]}#{self.cpf[11..12]}"
+    self.update_column(:cpf, formatted_cpf)
+  end
+
+  def translated_payment
+    if self.payment_method == "credit_card"
+      "Cartão de crédito"
+    else
+      "Boleto"
+    end
+  end
 end
